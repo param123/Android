e9816e9601db1c95d7c2d87627f7e4255ef47076
param123 <singh.prabhat@gmail.com>
2011-08-25 22:25:36 +0530

    metadata formating

--- src/com/pk/manager/FileUtil.java
+++ src/com/pk/manager/FileUtil.java
@@ -2,6 +2,7 @@ package com.pk.manager;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -47,22 +48,34 @@ public class FileUtil {
 		  boolean isReadable = file.canRead();
 		  boolean isExecutable = file.exists();
 		  StringBuilder sb = new StringBuilder();
-		  sb.append(timeFormat).append(" | ");
-		  if(isWritable){
-			  sb.append("rw");
-			  if(isExecutable){
-				  sb.append("-e");
-			  }
-		  }else if(isReadable){
-			  sb.append("r");
-			  if(isExecutable){
-				  sb.append("-e");
-			  }
-		  }else if(isExecutable){
-			  sb.append("e");
+		  if(file.isFile()){
+			  sb.append(humanReadableByteCount(file.length(), false));
+			  sb.append(" | ");
 		  }
+		  sb.append(timeFormat);
+//		  if(isWritable){
+//			  sb.append("rw");
+//			  if(isExecutable){
+//				  sb.append("-e");
+//			  }
+//		  }else if(isReadable){
+//			  sb.append("r");
+//			  if(isExecutable){
+//				  sb.append("-e");
+//			  }
+//		  }else if(isExecutable){
+//			  sb.append("e");
+//		  }
 		  return sb.toString();		  
 	  }
+	  
+	  public static String humanReadableByteCount(long bytes, boolean si) {
+		    int unit = si ? 1000 : 1024;
+		    if (bytes < unit) return bytes + " Bytes";
+		    int exp = (int) (Math.log(bytes) / Math.log(unit));
+		    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+		    return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+		}
 	  
 	  public static SimpleDateFormat getDateFormatter(){
 		  return new SimpleDateFormat("dd-MM-yyyy HH:mm:ss aaa");
