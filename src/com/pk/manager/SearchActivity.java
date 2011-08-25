dc7bbf5b5d5b82165a1de7dec280f14e27a3cfd2
param123 <singh.prabhat@gmail.com>
2011-08-25 23:13:50 +0530

    search options created

--- src/com/pk/manager/SearchActivity.java
+++ src/com/pk/manager/SearchActivity.java
@@ -2,6 +2,8 @@ package com.pk.manager;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchActivity extends Activity {
 	
@@ -9,7 +11,12 @@ public class SearchActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search);
-		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.file_search_options_array, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    spinner.setAdapter(adapter);
+	    spinner.setSelection(0);
 	}
 
 }
