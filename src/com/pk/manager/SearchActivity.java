e27c5b92e552542ca8e2b2acf5c7c2f36c99f39a
param123 <singh.prabhat@gmail.com>
2011-08-26 10:52:16 +0530

    search button and layout

--- src/com/pk/manager/SearchActivity.java
+++ src/com/pk/manager/SearchActivity.java
@@ -1,11 +1,19 @@
 package com.pk.manager;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SearchActivity extends Activity {
+import com.markupartist.android.widget.actionbar.R;
+
+public class SearchActivity extends ListActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,35 @@ public class SearchActivity extends Activity {
 	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    spinner.setAdapter(adapter);
 	    spinner.setSelection(0);
+	    
+	    
+	    
+	    //we can attach onClick in xml as well.
+	    Button searchButton = (Button)findViewById(R.id.searchBtn);
+	    attachListenerToBtn(searchButton);
 	}
-
+	
+	private final Activity getActivityContext(){
+		return this;
+	}
+	
+	private void attachListenerToBtn(Button btn){
+		btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	Toast.makeText(v.getContext(),
+                        "Hola..",
+                        Toast.LENGTH_SHORT).show();
+            	ListView lv = getListView();
+            	lv.setVisibility(View.VISIBLE);
+             	FileObject fo[] = new FileObject[]{new FileObject("Sample","/demo",true,"no metadata")};
+             	FileAdapter fa = new FileAdapter(getActivityContext(), R.layout.items,fo);
+             	setListAdapter(fa);
+             	
+            	
+            }
+        });
+	}
+	
+	
 }
